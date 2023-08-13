5508c02d94090b7bbfe92fbe609474d8ba71bed0
Aleksandar Brodschneider <aleksandar.stepanovic@almamater.si>
2023-08-13 23:38:05 +0200

    Dodane datoteke in mape za spletno stran

---
+++ public_html/script.js
@@ -0,0 +1,9 @@
+// script.js
+
+// Pridobitev gumba po ID-ju
+var button = document.getElementById('myButton');
+
+// Dodajanje poslušalca dogodka na gumb
+button.addEventListener('click', function() {
+  alert('Pozdravljeni! Dobrodošli na spletni strani Računalništvo v oblaku! Želimo Vam prijetno učenje :) ');
+});
